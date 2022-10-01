7e43ad563b606c64e610281883ccf13a308983cb
Igor Paranhos <igor.paranhos@arctouch.com>
2022-10-01 14:39:42 -0300

    Remove focus effect and add pwd capabilities

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-import './styles/style.css'
-import './styles/game.css'
+import "./styles/style.css";
+import "./styles/game.css";
 
-import { Renderer } from './src/Renderer'
+import { Renderer } from "./src/Renderer";
 
-const main = () => {
-  const $canvas = document.querySelector('#game');
-  const renderer = new Renderer($canvas);
-
-  const $start = document.querySelector('#start')
-  const $stop = document.querySelector('#stop')
-  $start.addEventListener('click', renderer.start)
-  $stop.addEventListener('click', renderer.stop)
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(registration => {
+    console.log('SW Registered!')
+    console.log(registration)
+  }).catch(error => {
+    console.log('SW Registration Error')
+    console.log(error)
+  })
 }
 
-window.addEventListener('DOMContentLoaded', main)
+const main = () => {
+  const $canvas = document.querySelector("#game");
+  const renderer = new Renderer($canvas);
+
+  const $start = document.querySelector("#start");
+  const $stop = document.querySelector("#stop");
+  $start.addEventListener("click", () => {
+    $start.disabled = true;
+    $stop.disabled = false;
+    renderer.start();
+  });
+  $stop.addEventListener("click", () => {
+    $start.disabled = false;
+    $stop.disabled = true;
+    renderer.stop();
+  });
+};
+
+window.addEventListener("DOMContentLoaded", main);
