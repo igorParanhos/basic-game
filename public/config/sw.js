717af0c7389133264719d278e390fce78079247a
Igor Paranhos <igor.paranhos@arctouch.com>
2022-10-02 15:54:19 -0300

    fix sw

--- public/config/sw.js
+++ public/config/sw.js
@@ -4,21 +4,11 @@ function wipeCache() {
   });
 }
 
-const images = [
-  "/images/android-chrome-192x192.png",
-  "/images/favicon-32x32.png",
-  "/images/safari-pinned-tab.svg",
-];
-const config = [
-    '/config/manifest.json',
-    '/config/browserconfig.xml',
-]
-
 self.addEventListener("install", (e) => {
   console.log("Installing");
   e.waitUntil(
     caches.open("static").then((cache) => {
-      return cache.addAll(["./assets", ...images, ...config]);
+      return cache.addAll(["./assets", './config']);
     })
   );
 });
