b13817413001bea592e66aa56c5dc44341530dd6
Igor Paranhos <igor.paranhos@arctouch.com>
2022-10-03 19:47:50 -0300

    Remove game logic from renderer layer

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,11 @@
+import { Enemy } from "./objects/Enemy";
+import { Player } from "./objects/Player";
+import { Prize } from "./objects/Prize";
 import { Level } from "./levels/Level";
 import { Renderer } from "./engine/Renderer";
 import Observer from "./utils/Observer";
+
+const $result = document.querySelector("#result");
 
 class Status extends Observer {
   constructor() {
@@ -43,18 +48,67 @@ export class Game {
     this.gameInfoProvider = new GameInfoProvider();
     this.gameInfoProvider.currentLevel = new Level();
     this.renderer = new Renderer(element, this.gameInfoProvider);
+    this._animationFrame = null
   }
   initialize = () => {
   };
   start = () => {
     this.gameInfoProvider.currentLevel.initialize();
-    this.renderer.start();
     this.gameInfoProvider.currentLevel.start();
     this.gameInfoProvider.status.start();
+    this.startRenderer()
   };
+
   stop = () => {
-    this.renderer.stop();
     this.gameInfoProvider.currentLevel.stop();
     this.gameInfoProvider.status.stop();
+    this.stopRenderer()
+  };
+
+  startRenderer = () => {
+    this._animationFrame = requestAnimationFrame(this.tick);
+  };
+  stopRenderer = () => {
+    cancelAnimationFrame(this._animationFrame);
+  };
+  tick = () => {
+    this.renderer.renderObjects();
+    this.checkCollision();
+
+    if (this.gameInfoProvider.status.value == "playing")
+      this._animationFrame = requestAnimationFrame(this.tick);
+  };
+
+  handlePlayerEnemyCollision = () => {
+    this.gameInfoProvider.status.stop()
+    $result.classList.add('animate', 'fail')
+    $result.innerHTML = "You lost";
+  };
+  handlePlayerPrizeCollision = () => {
+    this.gameInfoProvider.status.stop()
+    $result.classList.add('animate', 'success')
+    $result.innerHTML = "You won";
+  };
+  checkCollision = () => {
+    const objects = this.gameInfoProvider.currentLevel.getObjects();
+    const { x, y } = this.gameInfoProvider.currentLevel.player;
+
+    for (let object of objects) {
+      const { x: objectX, y: objectY } = object;
+      if (
+        x < objectX + 10 &&
+        x + 10 > objectX &&
+        y < objectY + 10 &&
+        10 + y > objectY
+      ) {
+        if (object instanceof Player) continue;
+        if (object instanceof Enemy) {
+          this.handlePlayerEnemyCollision();
+        }
+        if (object instanceof Prize) {
+          this.handlePlayerPrizeCollision();
+        }
+      }
+    }
   };
 }
--- src/engine/Renderer.js
+++ src/engine/Renderer.js
@@ -1,15 +1,10 @@
-import { Enemy } from "../objects/Enemy";
-import { Player } from "../objects/Player";
-import { Prize } from "../objects/Prize";
 import { Canvas2D } from "../Canvas2D";
 
 const CANVAS_SIZE = 500;
 
-const $result = document.querySelector("#result");
 
 export class Renderer {
   constructor($element, gameInfoProvider) {
-    this._interval = null;
     this.$element = $element;
     this.ctx = $element.getContext("2d");
     this.canvas = new Canvas2D(this.ctx, CANVAS_SIZE);
@@ -20,58 +15,11 @@ export class Renderer {
     this.gameInfoProvider = gameInfoProvider
   }
 
-  start = () => {
-    this._interval = requestAnimationFrame(this.tick);
-  };
-  stop = () => {
-    cancelAnimationFrame(this._interval);
-  };
-  tick = () => {
-    this.canvas.clear();
-    this.renderObjects();
-    this.checkCollision();
-
-    if (this.gameInfoProvider.status.value == "playing")
-      this._interval = requestAnimationFrame(this.tick);
-  };
-
   renderObjects = () => {
+    this.canvas.clear();
     for (let object of this.gameInfoProvider.currentLevel.getObjects()) {
       const { x, y } = object.getPosition();
       this.canvas.square(x, y, 10, object.color);
     }
-  };
-
-  checkCollision = () => {
-    const objects = this.gameInfoProvider.currentLevel.getObjects();
-    const { x, y } = this.gameInfoProvider.currentLevel.player;
-
-    for (let object of objects) {
-      const { x: objectX, y: objectY } = object;
-      if (
-        x < objectX + 10 &&
-        x + 10 > objectX &&
-        y < objectY + 10 &&
-        10 + y > objectY
-      ) {
-        if (object instanceof Player) continue;
-        if (object instanceof Enemy) {
-          this.handlePlayerEnemyCollision();
-        }
-        if (object instanceof Prize) {
-          this.handlePlayerPrizeCollision();
-        }
-      }
-    }
-  };
-  handlePlayerEnemyCollision = () => {
-    this.gameInfoProvider.status.stop()
-    $result.classList.add('animate', 'fail')
-    $result.innerHTML = "You lost";
-  };
-  handlePlayerPrizeCollision = () => {
-    this.gameInfoProvider.status.stop()
-    $result.classList.add('animate', 'success')
-    $result.innerHTML = "You won";
   };
 }
