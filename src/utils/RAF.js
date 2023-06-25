735f05e0e9f8acc4875a4405e1d2070910f119b0
Igor Paranhos <igor.paranhosto@gmail.com>
2023-06-25 02:22:49 -0300

    Comment fps logs

--- src/utils/RAF.js
+++ src/utils/RAF.js
@@ -1,8 +1,8 @@
-import Observer from './Observer'
+import Observer from './Observer';
 
 export class RAF extends Observer {
   constructor() {
-    super()
+    super();
     this.start = this.start.bind(this);
     this.pause = this.pause.bind(this);
     this.tick = this.tick.bind(this);
@@ -11,41 +11,37 @@ export class RAF extends Observer {
     this.time = null;
   }
 
-  emit = this._emit('tick')
-  subscribe = this._on('tick')
-  unsubscribe = this._cancel('tick')
+  emit = this._emit('tick');
+  subscribe = this._on('tick');
+  unsubscribe = this._cancel('tick');
 
   start = () => {
     this.isPaused = false;
     this._startTime = new Date().getTime();
     this._ticks = 0;
     this.tick(this.time);
-  }
+  };
   pause = () => {
     this.isPaused = true;
-  }
+  };
   tick = (now) => {
     this._ticks++;
 
-    const n = new Date().getTime();
-    const elapsed = n - this._startTime;
-      // console.log('FPS', this._ticks / elapsed * 1000);
-      // console.log('elapsed', elapsed);
-
-      console.log('FPS', this._ticks / elapsed * 1000);
+    // const n = new Date().getTime();
+    // const elapsed = n - this._startTime;
+    // console.log('FPS', this._ticks / elapsed * 1000);
 
     let delta = 0;
 
-    if (this.time)
-      delta = now - this.time;
+    if (this.time) delta = now - this.time;
 
     this.time = now;
 
     if (!this.isPaused) {
-      this.emit({ delta, now })
+      this.emit({ delta, now });
       requestAnimationFrame(this.tick);
     }
-  }
+  };
 }
 
 const _instance = new RAF();
